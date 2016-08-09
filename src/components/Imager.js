d45817e11f2c8a02276359b24dca81120594a9dc
bhsiung <bear2029@gmail.com>
2016-08-08 17:26:19 -0700

    adding unit test

--- src/components/Imager.js
+++ src/components/Imager.js
@@ -1,67 +1,108 @@
 import React, { PropTypes, PureComponent } from 'react'
 import _ from 'lodash'
+const RESTRICT_CLASS_VERTICAL = 'restrict-v'
+const RESTRICT_CLASS_HORIZONTAL = 'restrict-h'
 
 const IMAGER_PROPTYPES = {
   className: PropTypes.string,
+  crop: PropTypes.bool,
+  failedClass: PropTypes.string,
   height: PropTypes.number,
+  loadedClass: PropTypes.string,
+  loadingClass: PropTypes.string,
   onFailed: PropTypes.func,
   onLoaded: PropTypes.func,
   src: PropTypes.string.isRequired,
   width: PropTypes.number
 }
+const IMAGER_DEFAULT_PROPS = {
+  className: '',
+  failedClass: 'fail',
+  loadingClass: 'loading',
+  loadedClass: 'loaded'
+}
 export default class Imager extends PureComponent {
   constructor(props){
     super(props)
-    _.bindAll(this, 'onLoaded')
+    _.bindAll(this, 'onLoaded', 'onFailed')
     this.state = {
       loaded: false,
       error: undefined
     }
-    let img = new Image()
-    img.addEventListener("error", e => this.onFailed(e))
-    img.addEventListener("load", e => {
-      this.onLoaded(e, img.width, img.height)
-    })
-    img.src = this.props.src
   }
-  onLoaded(e, width, height){
-    if(_.isFunction(this.props.onLoaded)) this.props.onLoaded(e, { width, height })
+  onLoaded(e){
+    this.w = e.target.naturalWidth
+    this.h = e.target.naturalHeight
     this.setState({ loaded: true })
+    if(_.isFunction(this.props.onLoaded)) {
+      this.props.onLoaded(e, {
+        width: this.w,
+        height: this.h
+      })
+    }
   }
   onFailed(e){
     if(_.isFunction(this.props.onFailed)) this.props.onFailed(e)
     this.setState({ error: e })
   }
   render(){
-    let mainStyle, imgStyle, imgClass = ''
+    let mainStyle = {}, imgStyle = {}, mainClass = ''
     if(this.state.loaded){
-      imgClass += ' loaded'
+      mainClass += ' loaded'
+      if(this.props.crop){
+        if(!this.props.width || !this.props.height) {
+          throw new Error('width & height is required from props when crop is expected')
+        }
+        const { restrictedClass, offset } = getRestrictedClass(
+          this.w,
+          this.h,
+          this.props.width,
+          this.props.height
+        )
+        mainClass += ` ${restrictedClass}`
+        mainStyle.height = this.props.height
+        mainStyle.width = this.props.width
+        if(restrictedClass == RESTRICT_CLASS_HORIZONTAL){
+          imgStyle.marginLeft = `${offset}px`
+        }else{
+          imgStyle.marginTop = `${offset}px`
+        }
+      }
     }else if(this.state.error){
-      imgClass += ' failed'
+      mainClass += ' failed'
     }else{
-      imgStyle = {
-        position: 'absolute',
-        visibility: 'hidden',
-        top: 0,
-        left: 0
-      }
-      mainStyle = {
-        position: 'relative'
-      }
+      mainClass += ' loading'
     }
     return (
       <span
+        className={ `react-imager ${this.props.className} ${mainClass}` }
         style={ mainStyle }
-        className={ `react-imager this.props.className` }
       >
         <img
+          onError={ this.onFailed }
+          onLoad={ this.onLoaded }
           src={ this.props.src }
           style={ imgStyle }
-          className={ imgClass }
-          onLoad={ this.onLoaded }
         />
       </span>
     )
   }
 }
 Imager.propTypes = IMAGER_PROPTYPES
+Imager.defaultProps = IMAGER_DEFAULT_PROPS
+
+export function getRestrictedClass(w, h, targetWidth, targetHeight){
+  const ratioOriginal = w / h
+  const ratioTarget = targetWidth / targetHeight
+  if(ratioOriginal > ratioTarget){
+    return {
+      restrictedClass: RESTRICT_CLASS_HORIZONTAL,
+      offset: (targetWidth - w * targetHeight / h) / 2
+    }
+  }else{
+    return {
+      restrictedClass: RESTRICT_CLASS_VERTICAL,
+      offset: (targetHeight - h * targetWidth / w) / 2
+    }
+  }
+}
--- src/stories/imager.js
+++ src/stories/imager.js
@@ -2,13 +2,17 @@ import _ from 'lodash'
 import React from 'react'
 import Imager from '../components/Imager'
 import { storiesOf, action } from '@kadira/storybook'
+import  Highlight from 'react-highlight'
+import '../scss/imagr-theme.scss'
 storiesOf('basic', module)
   .add('load successfully', () => {
     return (
-      <Imager
-        src="http://fooding.hmgcdn.com/images/article/102659/a102659_108112_1431057831.jpg"
-        onLoaded={ action('loaded') }
-      />
+      <div>
+        <Imager
+          src="http://image.shutterstock.com/z/stock-photo-grandmother-with-mother-and-daughter-against-the-lake-144816268.jpg"
+          onLoaded={ action('loaded') }
+        />
+      </div>
     )
   })
   .add('fail to load', () => {
@@ -17,5 +21,57 @@ storiesOf('basic', module)
         src="http://www.yahoo.com/aa.jpg"
         onFailed={ action('loading failed') }
       />
+    )
+  })
+storiesOf('width style', module)
+  .add('load successfully', () => {
+    return (
+      <Imager
+        src="http://image.shutterstock.com/z/stock-vector-healthy-vegetables-frame-linear-graphic-vector-illustration-253355536.jpg"
+        onLoaded={ action('loaded') }
+        className="imagr example"
+      />
+    )
+  })
+  .add('fail to load', () => {
+    return (
+      <Imager
+        src="http://www.yahoo.com/aa.jpg"
+        onFailed={ action('loading failed') }
+        className="imagr example"
+      />
+    )
+  })
+  .add('crop square', () => {
+    return (
+        <Imager
+          crop
+          width={ 300 }
+          height={ 300 }
+          src="http://image.shutterstock.com/z/stock-vector-healthy-vegetables-frame-linear-graphic-vector-illustration-253355536.jpg"
+          className="imagr"
+        />
+    )
+  })
+  .add('crop landscape', () => {
+    return (
+        <Imager
+          crop
+          width={ 300 }
+          height={ 100 }
+          src="http://image.shutterstock.com/z/stock-vector-healthy-vegetables-frame-linear-graphic-vector-illustration-253355536.jpg"
+          className="imagr"
+        />
+    )
+  })
+  .add('crop portrait', () => {
+    return (
+        <Imager
+          crop
+          width={ 100 }
+          height={ 300 }
+          src="http://image.shutterstock.com/z/stock-vector-healthy-vegetables-frame-linear-graphic-vector-illustration-253355536.jpg"
+          className="imagr"
+        />
     )
   })
